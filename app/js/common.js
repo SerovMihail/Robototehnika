e3412350cf01eba7dd9165362d8acdbd7e4ce1ad
Serov Mihail <serov.m.888@icloud.com>
2017-03-12 23:54:43 +0300

    init commit. Production.

---
+++ app/js/common.js
@@ -0,0 +1,522 @@
+
+// script gallery
+function Go() {
+
+   // console.log('ready');
+    var initPhotoSwipeFromDOM = function(gallerySelector) {
+
+            var parseThumbnailElements = function(el) {
+                var thumbElements = el.childNodes,
+                    numNodes = thumbElements.length,
+                    items = [],
+                    el,
+                    childElements,
+                    thumbnailEl,
+                    size,
+                    item;
+
+                for(var i = 0; i < numNodes; i++) {
+                    el = thumbElements[i];
+
+                    // include only element nodes 
+                    if(el.nodeType !== 1) {
+                      continue;
+                    }
+
+                    childElements = el.children;
+
+                    size = el.getAttribute('data-size').split('x');
+
+                    // create slide object
+                    item = {
+                        src: el.getAttribute('href'),
+                        w: parseInt(size[0], 10),
+                        h: parseInt(size[1], 10),
+                        author: el.getAttribute('data-author')
+                    };
+
+                    item.el = el; // save link to element for getThumbBoundsFn
+
+                    if(childElements.length > 0) {
+                      item.msrc = childElements[0].getAttribute('src'); // thumbnail url
+                      if(childElements.length > 1) {
+                          item.title = childElements[1].innerHTML; // caption (contents of figure)
+                      }
+                    }
+
+
+                    var mediumSrc = el.getAttribute('data-med');
+                    if(mediumSrc) {
+                        size = el.getAttribute('data-med-size').split('x');
+                        // "medium-sized" image
+                        item.m = {
+                            src: mediumSrc,
+                            w: parseInt(size[0], 10),
+                            h: parseInt(size[1], 10)
+                        };
+                    }
+                    // original image
+                    item.o = {
+                        src: item.src,
+                        w: item.w,
+                        h: item.h
+                    };
+
+                    items.push(item);
+                }
+
+                return items;
+            };
+
+            // find nearest parent element
+            var closest = function closest(el, fn) {
+                return el && ( fn(el) ? el : closest(el.parentNode, fn) );
+            };
+
+            var onThumbnailsClick = function(e) {
+                e = e || window.event;
+                e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+                var eTarget = e.target || e.srcElement;
+
+                var clickedListItem = closest(eTarget, function(el) {
+                    return el.tagName === 'A';
+                });
+
+                if(!clickedListItem) {
+                    return;
+                }
+
+                var clickedGallery = clickedListItem.parentNode;
+
+                var childNodes = clickedListItem.parentNode.childNodes,
+                    numChildNodes = childNodes.length,
+                    nodeIndex = 0,
+                    index;
+
+                for (var i = 0; i < numChildNodes; i++) {
+                    if(childNodes[i].nodeType !== 1) { 
+                        continue; 
+                    }
+
+                    if(childNodes[i] === clickedListItem) {
+                        index = nodeIndex;
+                        break;
+                    }
+                    nodeIndex++;
+                }
+
+                if(index >= 0) {
+
+                    openPhotoSwipe( index, clickedGallery );
+                }
+                return false;
+            };
+
+            var photoswipeParseHash = function() {
+                var hash = window.location.hash.substring(1),
+                params = {};
+
+                if(hash.length < 5) { // pid=1
+                    return params;
+                }
+
+                var vars = hash.split('&');
+                for (var i = 0; i < vars.length; i++) {
+                    if(!vars[i]) {
+                        continue;
+                    }
+                    var pair = vars[i].split('=');  
+                    if(pair.length < 2) {
+                        continue;
+                    }           
+                    params[pair[0]] = pair[1];
+                }
+
+                if(params.gid) {
+                    params.gid = parseInt(params.gid, 10);
+                }
+
+                return params;
+            };
+
+            var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+                var pswpElement = document.querySelectorAll('.pswp')[0],
+                    gallery,
+                    options,
+                    items;
+
+                items = parseThumbnailElements(galleryElement);
+
+                // define options (if needed)
+                options = {
+
+                    galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+
+                    getThumbBoundsFn: function(index) {
+                        // See Options->getThumbBoundsFn section of docs for more info
+                        var thumbnail = items[index].el.children[0],
+                            pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+                            rect = thumbnail.getBoundingClientRect(); 
+
+                        return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+                    },
+
+                    addCaptionHTMLFn: function(item, captionEl, isFake) {
+                        if(!item.title) {
+                            captionEl.children[0].innerText = '';
+                            return false;
+                        }
+                        captionEl.children[0].innerHTML = item.title +  '<br/><small>Photo: ' + item.author + '</small>';
+                        return true;
+                    }
+                    
+                };
+
+
+                if(fromURL) {
+                    if(options.galleryPIDs) {
+                        // parse real index when custom PIDs are used 
+                        // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
+                        for(var j = 0; j < items.length; j++) {
+                            if(items[j].pid == index) {
+                                options.index = j;
+                                break;
+                            }
+                        }
+                    } else {
+                        options.index = parseInt(index, 10) - 1;
+                    }
+                } else {
+                    options.index = parseInt(index, 10);
+                }
+
+                // exit if index not found
+                if( isNaN(options.index) ) {
+                    return;
+                }
+
+
+
+                var radios = document.getElementsByName('gallery-style');
+                for (var i = 0, length = radios.length; i < length; i++) {
+                    if (radios[i].checked) {
+                        if(radios[i].id == 'radio-all-controls') {
+
+                        } else if(radios[i].id == 'radio-minimal-black') {
+                            options.mainClass = 'pswp--minimal--dark';
+                            options.barsSize = {top:0,bottom:0};
+                            options.captionEl = false;
+                            options.fullscreenEl = false;
+                            options.shareEl = false;
+                            options.bgOpacity = 0.85;
+                            options.tapToClose = true;
+                            options.tapToToggleControls = false;
+                        }
+                        break;
+                    }
+                }
+
+                if(disableAnimation) {
+                    options.showAnimationDuration = 0;
+                }
+
+                // Pass data to PhotoSwipe and initialize it
+                gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+
+                // see: http://photoswipe.com/documentation/responsive-images.html
+                var realViewportWidth,
+                    useLargeImages = false,
+                    firstResize = true,
+                    imageSrcWillChange;
+
+                gallery.listen('beforeResize', function() {
+
+                    var dpiRatio = window.devicePixelRatio ? window.devicePixelRatio : 1;
+                    dpiRatio = Math.min(dpiRatio, 2.5);
+                    realViewportWidth = gallery.viewportSize.x * dpiRatio;
+
+
+                    if(realViewportWidth >= 1200 || (!gallery.likelyTouchDevice && realViewportWidth > 800) || screen.width > 1200 ) {
+                        if(!useLargeImages) {
+                            useLargeImages = true;
+                            imageSrcWillChange = true;
+                        }
+                        
+                    } else {
+                        if(useLargeImages) {
+                            useLargeImages = false;
+                            imageSrcWillChange = true;
+                        }
+                    }
+
+                    if(imageSrcWillChange && !firstResize) {
+                        gallery.invalidateCurrItems();
+                    }
+
+                    if(firstResize) {
+                        firstResize = false;
+                    }
+
+                    imageSrcWillChange = false;
+
+                });
+
+                gallery.listen('gettingData', function(index, item) {
+                    if( useLargeImages ) {
+                        item.src = item.o.src;
+                        item.w = item.o.w;
+                        item.h = item.o.h;
+                    } else {
+                        item.src = item.m.src;
+                        item.w = item.m.w;
+                        item.h = item.m.h;
+                    }
+                });
+
+                gallery.init();
+            };
+
+            // select all gallery elements
+            var galleryElements = document.querySelectorAll( gallerySelector );
+            for(var i = 0, l = galleryElements.length; i < l; i++) {
+                galleryElements[i].setAttribute('data-pswp-uid', i+1);
+                galleryElements[i].onclick = onThumbnailsClick;
+            }
+
+            // Parse URL and open gallery if it contains #&pid=3&gid=1
+            var hashData = photoswipeParseHash();
+            if(hashData.pid && hashData.gid) {
+                openPhotoSwipe( hashData.pid,  galleryElements[ hashData.gid - 1 ], true, true );
+            }
+        };
+        
+        initPhotoSwipeFromDOM('.demo-gallery');
+
+};
+        
+    
+
+// $( document ).ready( Go() );
+// $('#main').bind("ajaxComplete", Go);
+
+
+// $('ul.accordion li a,ul.accordion li ul li a').on('click', Go());
+// end script gallery
+
+
+
+// LEFT SLIDER FOR MENU ITEM
+// !!!!!!!!!!!!!!!!!!!!!!!!!
+
+$(document).ready(function() {
+  
+/* =Reflection Nav
+-------------------------------------------------------------------------- */	
+	
+	// Append span to each LI to add reflection
+	
+	$(".slideRight").append("<span></span>");	
+ 
+
+	
+	// Animate buttons, move reflection and fade
+	
+	$(".slideRight").hover(function() {
+	    $(this).stop().animate({ marginLeft: "10px" }, 200);
+	    $(this).parent().find("span").stop().animate({ marginTop: "1px", opacity: 0.25, textAlign:"center" }, 200);
+	},function(){
+	    $(this).stop().animate({ marginLeft: "0px" }, 300);
+	    $(this).parent().find("span").stop().animate({ marginTop: "1px", opacity: 1,  textAlign:"center" }, 300);
+	});
+
+	
+
+
+});
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// END LEFT SLIDER FOR MENU ITEM 
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// SNIPPET FOR REGISTRATION
+
+/*
+ *
+ * login-register modal
+ * Autor: Creative Tim
+ * Web-autor: creative.tim
+ * Web script: http://creative-tim.com
+ * 
+ */
+function showRegisterForm(){
+    $('.loginBox').fadeOut('fast',function(){
+        $('.registerBox').fadeIn('fast');
+        $('.login-footer').fadeOut('fast',function(){
+            $('.register-footer').fadeIn('fast');
+        });
+
+       
+        $('.modal-title').html('Зарегистрироваться');
+    }); 
+    $('.error').removeClass('alert alert-danger').html('');
+       
+}
+function showLoginForm(){
+    $('#loginModal .registerBox').fadeOut('fast',function(){
+        $('.loginBox').fadeIn('fast');
+        $('.register-footer').fadeOut('fast',function(){
+            $('.login-footer').fadeIn('fast');    
+        });
+        
+       
+          
+        $('.modal-title').html('Войти');
+    });       
+     $('.error').removeClass('alert alert-danger').html(''); 
+}
+
+function openLoginModal(){
+    
+      
+    
+    showLoginForm();
+    setTimeout(function(){     
+        $('#loginModal').modal('show');    
+    }, 230);
+    
+}
+function openRegisterModal(){
+    
+    showRegisterForm();
+    setTimeout(function(){
+        $('#loginModal').modal('show');    
+    }, 230);
+}
+
+
+function loginAjax(){
+       // Remove this comments when moving to server
+    $.post( "/login", function( data ) {
+            if(data == 1){
+                window.location.replace("/home");            
+            } else {
+                 shakeModal(); 
+            }
+        });
+    
+
+/*   Simulate error message from the server   */
+     shakeModal();
+}
+
+function shakeModal(){
+    $('#loginModal .modal-dialog').addClass('shake');
+             $('.error').addClass('alert alert-danger').html("Такого аккаунта не существует");
+             $('input[type="password"]').val('');
+             setTimeout( function(){ 
+                $('#loginModal .modal-dialog').removeClass('shake'); 
+    }, 1000 ); 
+}
+
+// END SNIPPET FOR REGISTRATION
+
+
+// Load content with AJAX
+
+$(document).ready(function() {
+
+
+
+    // Check for hash value in URL
+    var hash = window.location.hash.substr(1);
+    var href = $('ul.nav li.slideRight a, ul.nav li ul li a').each(function(){
+        var href = $(this).attr('href');
+        if(hash==href.substr(0,href.length-5)){
+            var toLoad = hash+'.html #main';
+            $('#main').load(toLoad)
+        }
+    });
+
+    $('ul.accordion li a,ul.accordion li ul li a').click(function(){
+
+        var toLoad = $(this).attr('href')+' #main';
+        $('#main').hide('fast',loadContent);
+        $('#loader').remove();
+        $('.wrapper').append('<span id="loader">LOADING...</span>');
+        $('#loader').fadeIn('normal');
+        window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5);
+        function loadContent() {
+
+            $('#main').load(toLoad,'',showNewContent())
+
+        }
+        function showNewContent() { 
+
+            $('#main').show('normal',hideLoader());
+
+        }
+        function hideLoader() {
+
+            $('#loader').fadeOut('normal');
+
+            // loadGallery();
+        }
+        
+
+       
+
+        return false;
+        }
+
+    );
+
+    
+
+});
+
+// ENd load content with ajax
+
+
+// Start toggle menu script 
+$(function() {
+    var Accordion = function(el, multiple) {
+        this.el = el || {};
+        this.multiple = multiple || false;
+
+        // Variables privadas
+        var links = this.el.find('.link');
+        // Evento
+        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+    }
+
+    Accordion.prototype.dropdown = function(e) {
+        var $el = e.data.el;
+        $this = $(this),
+        $next = $this.next();
+
+        $next.slideToggle();
+        $this.parent().toggleClass('open');
+
+        if (!e.data.multiple) {
+            $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+        };
+    }   
+
+    var accordion = new Accordion($('#accordion'), false);
+});
+
+// end toggle menu script
+
+// start toggle div content 
+
+$(document).ready(function(){
+   $('.spoiler_links').click(function(){
+        $('.spoiler_body').slideToggle();
+        return false;     
+  });
+
+});
+
+// end toggle div content
+
